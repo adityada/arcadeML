c3ece31e28d458bb774367617506c40f76bf874a
adityada <40657392+adityada@users.noreply.github.com>
2019-02-13 20:44:17 +0700

    Update bird.js

--- games/flappybirdML/bird.js
+++ games/flappybirdML/bird.js
@@ -21,7 +21,7 @@ class Bird {
     show() {
         fill(255, 50)
         stroke(255)
-        image(this.sprite, this.x, this.y, this.sprite.width * 1,2, this.sprite.height * 1.2)
+        image(this.sprite, this.x, this.y, this.sprite.width * 1.2, this.sprite.height * 1.2)
     }
 
     up() {
